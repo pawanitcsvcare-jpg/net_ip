1255cc6f78bc24cebb0b8d727c563b33ee446c9a
pawanitcsvcare-jpg <pawan.itcsvcare@gmail.com>
2026-02-17 17:27:00 +0530

    change

--- src/components/DonutChart.tsx
+++ src/components/DonutChart.tsx
@@ -14,49 +14,50 @@ export default function DonutChart() {
       type: "donut", 
       height: 350 
     },
-    plotOptions: {
-      pie: {
-        size: 100,
-        offsetX: 0,
-        offsetY: 0,
-        donut: {
-          size: "77%",
-          labels: {
-            show: true,
-            name: { 
-              show: true, 
-              fontSize: "18px", 
-              offsetY: -5 
-            },
-            value: {
-              show: true,
-              fontSize: "24px",
-              color: "#343a40",
-              fontWeight: 500,
-              offsetY: 10,
-              formatter: function (val: string) {
-                return "" + val;
-              },
-            },
-            total: {
-              show: true,
-              fontSize: "16px",
-              label: "Total",
-              color: "#9599ad",
-              fontWeight: 400,
-              formatter: function (w: any) {
-                return (
-                  "" +
-                  w.globals.seriesTotals.reduce(function (a: number, b: number) {
-                    return a + b;
-                  }, 0)
-                );
-              },
-            },
+  plotOptions: {
+  pie: {
+    customScale: 0.9, // ✅ replaces size
+    offsetX: 0,
+    offsetY: 0,
+    donut: {
+      size: "77%",
+      labels: {
+        show: true,
+        name: { 
+          show: true, 
+          fontSize: "18px", 
+          offsetY: -5 
+        },
+        value: {
+          show: true,
+          fontSize: "24px",
+          color: "#343a40",
+          fontWeight: 500,
+          offsetY: 10,
+          formatter: function (val: string) {
+            return "" + val;
+          },
+        },
+        total: {
+          show: true,
+          fontSize: "16px",
+          label: "Total",
+          color: "#9599ad",
+          fontWeight: 400,
+          formatter: function (w: any) {
+            return (
+              "" +
+              w.globals.seriesTotals.reduce(function (a: number, b: number) {
+                return a + b;
+              }, 0)
+            );
           },
         },
       },
     },
+  },
+},
+
     dataLabels: { enabled: false },
     legend: { show: true, position: "bottom" },
     yaxis: {
